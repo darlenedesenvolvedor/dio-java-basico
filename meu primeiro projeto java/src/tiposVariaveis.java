9f248ae0368fc3a2fce09b5ff7b0eea9415328ff
Darlene Moura <mdarlenemoura@gmail.com>
2022-12-22 10:45:13 -0300

    Commit inicial

---
+++ meu primeiro projeto java/src/tiposVariaveis.java
@@ -0,0 +1,16 @@
+public class tiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        
+
+        String meuNome= "Darlene Moura";
+
+        System.out.println(meuNome);
+
+        int anoNascimento= 1992;
+        int anoAtual= 2022;
+        
+        int soma_Idade= (anoAtual - anoNascimento);
+
+        System.out.println(soma_Idade);
+    }
+}
