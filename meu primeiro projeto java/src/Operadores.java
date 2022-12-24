983b29011f270403a426e943b77016bd61bd6122
Darlene Moura <mdarlenemoura@gmail.com>
2022-12-24 10:28:39 -0300

    Commit inicial

---
+++ meu primeiro projeto java/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    
+    public static void main(String[] args) {  
+     int numero= 5;
+     // repetição
+     numero= numero + 2;
+
+        System.out.println(numero);
+}
+}
+
